a096d267eeb940eb09a5a82e400a86d1cc9cd043
Jazgalor <jazgalor@wp.pl>
2026-02-23 16:07:04 +0100

    SGD Momentum for no-torch

---
+++ no_torch/momentum.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+
+class SGDMomentum:
+
+    def __init__(self, lr=1e-3, beta=0.9):
+
+        self.lr = lr
+        self.beta = beta
+        self.step = 0
+        self.state = {}
+
+
+    def init_state(self, param):
+
+        return {
+            "m": np.zeros_like(param)
+        }
+
+
+    def compute_update(self, param, grad, state):
+
+        m = state["m"]
+
+        m = self.beta * m + grad
+
+        state["m"] = m
+
+        update = m
+
+        return update
+
+
+    def apply_update(self, param, update):
+
+        param -= self.lr * update
+
+
+    def step_param(self, param, grad):
+
+        param_id = id(param)
+
+        if param_id not in self.state:
+            self.state[param_id] = self.init_state(param)
+
+        state = self.state[param_id]
+
+        update = self.compute_update(param, grad, state)
+
+        self.apply_update(param, update)
+
+
+    def step(self, params, grads):
+
+        self.step += 1
+
+        for param, grad in zip(params, grads):
+
+            self.step_param(param, grad)
